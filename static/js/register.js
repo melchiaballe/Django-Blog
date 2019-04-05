215b7226a6d15e997a4365f0fe3782dd19944587
melchiaballe <melchiaballe@gmail.com>
2019-04-05 15:48:46 +0800

    //Final Demo Build

--- static/js/register.js
+++ static/js/register.js
@@ -20,23 +20,29 @@ $(document).ready(function(){
                 if(err.password){
                     $('#password').attr('class', 'form-control is-invalid')
                     $('#password2').attr('class', 'form-control is-invalid')
+                    newpass = err.password.join(" ");
+                    $('#invalid_password').html(newpass);
                 }
                 else{
                     $('#password').attr('class', 'form-control is-valid')
                     $('#password2').attr('class', 'form-control is-valid')
+                    $('#invalid_password').empty();
                 }
 
                 if(err.email){
                     $('#email').attr('class', 'form-control is-invalid')
+                    $('#invalid_email').html(err.email)
                 }
                 else{
                     $('#email').attr('class', 'form-control is-valid')
+                    $('#invalid_email').empty()
                 }
             })
         }
         else{
             $('#password').attr('class', 'form-control is-invalid')
             $('#password2').attr('class', 'form-control is-invalid')
+            $('#invalid_password').html("password do not match")
         }
     })
 })
